bdb06f5fc5f15feb17a1e35ddb67baf1ef042c18
Andrew Hampson <andrewhampson6@gmail.com>
2024-01-16 13:32:34 +0400

    Explore dynamic route definition and provide a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'demo/index'
+  # get ':controller(/:action(/:id))'
+  # This dynamic default route will be depricated in Rails 7.2 ... do not use
+  #
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "demo#index"
 end
